0205d8c28109500d2d81f0b8e0a9be322028ba46
Tyler Nie <tylernie@live.ca>
2020-07-23 16:15:48 -0700

    retrieves data from dictionary using strings

--- movie recommendation.py
+++ movie recommendation.py
@@ -2,5 +2,9 @@ import numpy as mp
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
-# Fetch data and format it
+# Fetch data and format it (creates an interaction matrix and stores the data in our data variable as a dictionary)
 data = fetch_movielens(min_rating=4.0)
+
+# Print training and testing data
+print(repr(data['train']))
+print(repr(data['test']))
